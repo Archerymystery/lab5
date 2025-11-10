9044cfc6d4bbe2f4efcbc58bef836ae4d253d5f6
Archerymystery <ratibor200805@gmail.com>
2025-11-10 17:00:18 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# LAB 5
+
+## Build and run project
+### Linux
+```bash
+mkdir build
+cd build
+cmake ..
+cd .. 
+cmake --build build/
+./build/main
+```
---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "C++ Development Shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cmake       
+            gcc         
+            gtest
+          ];
+
+       shellHook = ''
+  
+            exec ${pkgs.fish}/bin/fish
+          '';
+        };
+      }
+    );
+}
