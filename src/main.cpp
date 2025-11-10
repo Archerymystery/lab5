ada3551baf8388530b129922ba1d06be9f788df1
Archerymystery <ratibor200805@gmail.com>
2025-11-10 18:40:32 +0200

    Update of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,90 +1,100 @@
 #include <iostream>
-#include <fstream>
+#include <cmath>
 using namespace std;
-int main()
-{
-    double a, b, h, y;
-    bool f;
-    int n;
-    ofstream fout("result.txt");
 
-    cout << "a=";
-    cin >> a;
-    cout << "b=";
-    cin >> b;
-    cout << "h=";
-    cin >> h;
-    cout << "n=";
-    cin >> n;
-    cout << "whether to write to a file (1 yes, 0 no): ";
-    cin >> f;
-    if (a >= b)
+void checkValidInput()
+{
+    if (cin.fail())
     {
-        cout << "a must be less then b";
-        return 0;
+        throw "Incorect data";
     }
-    if (b - a <= h)
-    {
-        cout << "step must be less then interval";
-        return 0;
-    }
+}
+
+void checkValidParams(int n)
+{
     if (n < 1)
     {
-        cout << "n must be greater then 1";
-        return 0;
+        throw "n must be greater then 1";
     }
-    if (f)
-    {
-        fout << "interval: (" << a << ";" << b << ") step: " << h << " n:" << n << endl;
-    }
-    double x = a;
-    while (x <= b)
-    {
-
-        if (x > 0)
-        {
-            y = 1;
-
-            for (double i = 1; i <= n; i++)
-            {
-                y = y * ((1 / x) - (1 / i));
-            }
-            if (f)
-            {
-                fout << "x: " << x << "\ny: " << y << "\n\n";
-            }
-            cout << "x: " << x << "\ny: " << y << "\n\n";
-        }
-        else
-        {
-            y = 0;
-            bool error = false;
-            for (int i = 0; i < n && !error; i++)
-            {
-                for (int j = 0; j < n && !error; j++)
-                {
-                    if ((x - i + x * j) == 0)
-                    {
-                        if (f)
-                        {
-                            fout << "x: " << x << "\ny: undefind \n\n";
-                        }
-                        cout << "x: " << x << "\ny: undefind \n\n";
-                        error = true;
-                    }
-                    y += 1 / (x - i + x * j);
-                }
-            }
-            if (!error)
-            {
-                if (f)
-                {
-                    fout << "x: " << x << "\ny: " << y << "\n\n";
-                }
-                cout << "x: " << x << "\ny: " << y << "\n\n";
-            }
-        }
-        x += h;
-    }
-    fout.close();
 }
+double calculate(int n, double x)
+{
+    double y;
+    if (x > 0)
+    {
+        y = 1;
+
+        for (double i = 1; i <= n; i++)
+        {
+            y = y * ((1 / x) - (1 / i));
+        }
+    }
+    else
+    {
+        y = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if ((x - i + x * j) == 0)
+                {
+
+                    return INFINITY;
+                }
+                y += 1 / (x - i + x * j);
+            }
+        }
+    }
+    return y;
+}
+
+#ifndef UNIT_TEST
+int main()
+{
+
+    try
+
+    {
+
+        double x, y;
+
+        int n;
+
+        cout << "n=";
+
+        cin >> n;
+
+        checkValidInput();
+
+        checkValidParams(n);
+
+        cout << "x=";
+
+        cin >> x;
+
+        checkValidInput();
+
+        cout << "x: " << x << " y: " << calculate(n, x) << endl;
+    }
+
+    catch (const char *ex)
+
+    {
+
+        cout << ex << endl;
+
+        return -1;
+    }
+
+    catch (...)
+
+    {
+
+        cout << "Unknown error\n";
+
+        return -2;
+    }
+}
+
+#endif
